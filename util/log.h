2d65359e3a27ae9fb8a2be7bd21b9317e7b0efb7
orientye <orientye@qq.com>
2021-01-12 22:46:16 +0800

    null msg

--- util/log.h
+++ util/log.h
@@ -1,4 +1,16 @@
 #ifdef _LOG_H_
 
+class Log {
+    enum class LogLevel {
+        kTrace = 0,
+        kDebug,
+        kInfo,
+        kWarn,
+        kError,
+        kFatal,
+    }
+public:
+    void setLogLevel(Log::LogLevel l)
+}
 
 #endif //_LOG_H_
