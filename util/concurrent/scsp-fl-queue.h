62c8c984b494ad5805a11887a9fb01c037e64c9e
orientye <orient@localhost.localdomain>
2021-02-23 19:18:56 +0800

    queue

--- util/concurrent/scsp-fl-queue.h
+++ util/concurrent/scsp-fl-queue.h
@@ -1,8 +1,21 @@
 #ifdef _SCSP_FIXED_LENGTH_QUEUE_H_
 
 template<typename T>
-class SCSPFixedLengthQueue {
-
+struct SCSPFixedLengthQueue {
+    SCSPFixedLengthQueue(const SCSPFixedLengthQueue&) = delete;
+    SCSPFixedLengthQueue& operator=(const SCSPFixedLengthQueue&) = delete;
+    SCSPFixedLengthQueue(const SCSPFixedLengthQueue&&) = delete;
+    SCSPFixedLengthQueue& operator=(const SCSPFixedLengthQueue&&) = delete;
+    
+    SCSPFixedLengthQueue(std::uint32_t size) {
+    
+    }
+    
+    ~SCSPFixedLengthQueue() {
+    
+    }
+private:
+    std::uint32_t size_;
 }
 
 #endif //_SCSP_FIXED_LENGTH_QUEUE_H_
