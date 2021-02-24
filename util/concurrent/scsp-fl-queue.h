d37a21d002712f9ad7fce9e2ce7a3f01a2fe7474
orient <orientye@qq.com>
2021-02-24 22:16:04 +0800

    queue

--- util/concurrent/scsp-fl-queue.h
+++ util/concurrent/scsp-fl-queue.h
@@ -1,5 +1,7 @@
 #ifdef _SCSP_FIXED_LENGTH_QUEUE_H_
 
+#include <atomic>
+
 template<typename T>
 struct SCSPFixedLengthQueue {
     SCSPFixedLengthQueue(std::uint32_t capacity) {
@@ -23,16 +25,27 @@ struct SCSPFixedLengthQueue {
     
     }
 
-    T pop() {
-    	return T;
+    T* front() {
+    
+    }
+
+    void pop() {
+
     }
     
-    bool is_full() {
-    	return false;
+    bool full() {
+       auto next = write_idx_.load(std::memory_order_acquire) + 1;
+       if (next == capacity_) {
+          next = 0;
+       }
+       if (next != read_idx_.load(std::memory_order_acquire)) {
+          return false;
+       }
+       return true;
     }
     
-    bool is_emty() {
-    	return false;
+    bool empty() {
+        return read_idx_.load(std::memory_order_acquire) == write_idx_.load(std::memory_order_acquire);
     }
     
     void clear() {
@@ -40,10 +53,13 @@ struct SCSPFixedLengthQueue {
     }
     
     std::uint32_t capacity() {
-    	return 0;
+    	return capacity_;
     }
 private:
+    T* data_;
     std::uint32_t capacity_;
+    std::atomic<std::uint32_t> read_idx_;
+    std::atomic<std::uint32_t> write_idx_;
 }
 
 #endif //_SCSP_FIXED_LENGTH_QUEUE_H_
