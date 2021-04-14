d1416d3fd64e25bc063167f3aee1a95cb913853a
orient <orientye@qq.com>
2021-04-14 21:46:22 +0800

    queue

--- test/scsp-fl-queue-test.cc
+++ test/scsp-fl-queue-test.cc
@@ -1,19 +1,43 @@
-#include "doctest/doctest.h"
-
 #include "util/concurrent/scsp-fl-queue.h"
 
-TEST_CASE("SCSPFQ correctness") {
+#include <atomic>
+#include <thread>
+#include <vector>
 
-}
+#include "doctest/doctest.h"
 
-TEST_CASE("SCSPFQ perf") {
+namespace {
 
-}
+template <typename T>
+class CorrectTest {
+ public:
+  CorrectTest() {}
 
-TEST_CASE("SCSPFQ destructor") {
-    
-}
+  void operator()() {
+    std::thread producer([this] { this->produce(); });
+    std::thread consumer([this] { this->consume(); });
+    producer.join();
+    produce_finish_ = true;
+    consumer.join();
+  }
 
-TEST_CASE("SCSPFQ empty and full") {
+  void produce() {}
 
-}
+  void consume() {}
+
+ private:
+  SCSPFixedLengthQueue<T> queue_;
+  std::vector<T> testdata_;
+  std::atomic<bool> produce_finish_;
+};
+
+}  // namespace
+
+//////////////////////////////////////////////////////////////////////
+TEST_CASE("SCSPFQ correct") {}
+
+TEST_CASE("SCSPFQ perf") {}
+
+TEST_CASE("SCSPFQ destructor") {}
+
+TEST_CASE("SCSPFQ empty and full") {}
