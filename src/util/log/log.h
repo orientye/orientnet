510af43110b867c48db56a8d2889bb5dfeb64721
orient <orientye@qq.com>
2021-04-21 22:25:50 +0800

    daily

--- src/util/log/log.h
+++ src/util/log/log.h
@@ -1,14 +1,25 @@
-#ifdef _LOG_H_
+#ifndef _LOG_H_
+#define _LOG_H_
+
+#include <cstdint>
 
 class Log {
-    enum class LogLevel : std::int8_t {kTrace = 0, kDebug, kInfo, kWarn, kError, kFatal};
-public:
-    void trace(const char *s);
-    void debug(const char *s);
-    void info(const char *s);
-    void warn(const char *s);
-    void error(const char *s);
-    void fatal(const char *s);
-}
+  enum class LogLevel : std::int8_t {
+    kTrace = 0,
+    kDebug,
+    kInfo,
+    kWarn,
+    kError,
+    kFatal
+  };
 
-#endif //_LOG_H_
+ public:
+  void trace(const char *s);
+  void debug(const char *s);
+  void info(const char *s);
+  void warn(const char *s);
+  void error(const char *s);
+  void fatal(const char *s);
+};
+
+#endif  //_LOG_H_
