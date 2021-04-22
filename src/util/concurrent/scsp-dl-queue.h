3862be6379156c563fdee3371b338939bdc229d2
orient <orientye@qq.com>
2021-04-22 22:10:09 +0800

    queue

--- src/util/concurrent/scsp-dl-queue.h
+++ src/util/concurrent/scsp-dl-queue.h
@@ -95,21 +95,18 @@ struct SCSPDynamicLengthQueue {
   std::atomic<std::uint32_t> write_idx_;
 };
 
-
 // load with 'consume' (data-dependent) memory ordering
-template<typename T>
-T load_consume(T const* addr)
-{//hardware fence is implicit on x86
+template <typename T>
+T load_consume(T const* addr) {  // hardware fence is implicit on x86
   T v = *const_cast<T const volatile*>(addr);
-  __memory_barrier(); // compiler fence
+  __memory_barrier();  // compiler fence
   return v;
 }
 
 // store with 'release' memory ordering
-template<typename T>
-void store_release(T* addr, T v)
-{//hardware fence is implicit on x86
-  __memory_barrier(); // compiler fence
+template <typename T>
+void store_release(T* addr, T v) {  // hardware fence is implicit on x86
+  __memory_barrier();               // compiler fence
   *const_cast<T volatile*>(addr) = v;
 }
 
@@ -117,92 +114,78 @@ void store_release(T* addr, T v)
 size_t const cache_line_size = 64;
 
 // single-producer/single-consumer queue
-template<typename T>
-class spsc_queue
-{
-public:
-  spsc_queue()
-  {
-      node* n = new node;
-      n->next_ = 0;
-      tail_ = head_ = first_= tail_copy_ = n;
+template <typename T>
+class spsc_queue {
+ public:
+  spsc_queue() {
+    node* n = new node;
+    n->next_ = 0;
+    tail_ = head_ = first_ = tail_copy_ = n;
   }
 
-  ~spsc_queue()
-  {
-      node* n = first_;
-      do
-      {
-          node* next = n->next_;
-          delete n;
-          n = next;
-      }
-      while (n);
+  ~spsc_queue() {
+    node* n = first_;
+    do {
+      node* next = n->next_;
+      delete n;
+      n = next;
+    } while (n);
   }
 
-  void enqueue(T v)
-  {
-      node* n = alloc_node();
-      n->next_ = 0;
-      n->value_ = v;
-      store_release(&head_->next_, n);
-      head_ = n;
+  void enqueue(T v) {
+    node* n = alloc_node();
+    n->next_ = 0;
+    n->value_ = v;
+    store_release(&head_->next_, n);
+    head_ = n;
   }
-//returns 'false' if queue is empty
-  bool dequeue(T& v)
-  {
-      if (load_consume(&tail_->next_))
-      {
-          v = tail_->next_->value_;
-          store_release(&tail_, tail_->next_);
-          return true;
-      }
-      else
-      {
-          return false;
-      }
+  // returns 'false' if queue is empty
+  bool dequeue(T& v) {
+    if (load_consume(&tail_->next_)) {
+      v = tail_->next_->value_;
+      store_release(&tail_, tail_->next_);
+      return true;
+    } else {
+      return false;
+    }
   }
 
-private:internal node structure
-  struct node
-  {
-      node* next_;
-      T value_;
+ private:  // internal node structure
+  struct node {
+    node* next_;
+    T value_;
   };
-//consumer partaccessed mainly by consumer, infrequently be producer
-  node* tail_; // tail of the queue
-//delimiter between consumer part and producer part,so that they situated on different cache lines
-  char cache_line_pad_ [cache_line_size];
-//producer partaccessed only by producer
-  node* head_; // head of the queue
-  node* first_; // last unused node (tail of node cache)
-  node* tail_copy_; // helper (points somewhere between first_ and tail_)
+  // consumer partaccessed mainly by consumer, infrequently be producer
+  node* tail_;  // tail of the queue
+  // delimiter between consumer part and producer part,so that they situated on
+  // different cache lines
+  char cache_line_pad_[cache_line_size];
+  // producer partaccessed only by producer
+  node* head_;       // head of the queue
+  node* first_;      // last unused node (tail of node cache)
+  node* tail_copy_;  // helper (points somewhere between first_ and tail_)
 
-  node* alloc_node()
-  {
-      // first tries to allocate node from internal node cache,
-      // if attempt fails, allocates node via ::operator new()
+  node* alloc_node() {
+    // first tries to allocate node from internal node cache,
+    // if attempt fails, allocates node via ::operator new()
 
-      if (first_ != tail_copy_)
-      {
-          node* n = first_;
-          first_ = first_->next_;
-          return n;
-      }
-      tail_copy_ = load_consume(&tail_);
-      if (first_ != tail_copy_)
-      {
-          node* n = first_;
-          first_ = first_->next_;
-          return n;
-      }
-      node* n = new node;
+    if (first_ != tail_copy_) {
+      node* n = first_;
+      first_ = first_->next_;
       return n;
+    }
+    tail_copy_ = load_consume(&tail_);
+    if (first_ != tail_copy_) {
+      node* n = first_;
+      first_ = first_->next_;
+      return n;
+    }
+    node* n = new node;
+    return n;
   }
 
   spsc_queue(spsc_queue const&);
-  spsc_queue& operator = (spsc_queue const&);
+  spsc_queue& operator=(spsc_queue const&);
 };
 
 #endif  //_SCSP_DYNAMIC_LENGTH_QUEUE_H_
-
