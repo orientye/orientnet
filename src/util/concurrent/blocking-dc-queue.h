4f0d15c99791477b59b2d3c50f4f8f614069da85
orient <orientye@qq.com>
2021-06-07 21:22:31 +0800

    queue

---
+++ src/util/concurrent/blocking-dc-queue.h
@@ -0,0 +1,8 @@
+#ifndef _BLOCING_DYNAMIC_CAPACITY_QUEUE_H_
+#define _BLOCING_DYNAMIC_CAPACITY_QUEUE_H_
+
+template <typename T>
+struct BlockingDynamicCapacity {
+};
+
+#endif  //_BLOCING_DYNAMIC_CAPACITY_QUEUE_H_
---
+++ src/util/concurrent/blocking-fc-queue.h
@@ -0,0 +1,8 @@
+#ifndef _BLOCKGING_FIXED_CAPACITY_QUEUE_H_
+#define _BLOCKGING_FIXED_CAPACITY_QUEUE_H_
+
+template <typename T>
+struct BlockingFixedCapacityQueue {
+};
+
+#endif  //_BLOCKGING_FIXED_CAPACITY_QUEUE_H_
