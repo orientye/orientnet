9c4fad38d8dc57107e60ceeea3ad1818868cc617
orient <orientye@qq.com>
2021-06-08 22:14:36 +0800

    queue

--- src/util/concurrent/blocking-queue.h
+++ src/util/concurrent/blocking-queue.h
@@ -1,7 +1,9 @@
 #ifndef _BLOCING_QUEUE_H_
 #define _BLOCING_QUEUE_H_
 
+#include <condition_variable>
 #include <deque>
+#include <mutex>
 
 template <typename T>
 struct BlockingQueue {
@@ -9,9 +11,18 @@ struct BlockingQueue {
 
   ~BlockingQueue() {}
 
-  void enqueue(T&& v) {}
+  void enqueue(T&& v) {
+    {
+      std::unique_lock<std::mutex> lk(mutex_);
+      queue_.push_back(std::move(x));
+    }
+    cv_not_empty.notify_all();
+  }
 
   T dequeue() {
+    std::unique_lock<std::mutex> lk(mutex_);
+    cv_not_empty.wait(lk, [] { return !queue_.empty() });
+    assert(!queue_.empty());
     T front(std::move(queue_.front()));
     queue_.pop_front();
     return front;
@@ -24,6 +35,8 @@ struct BlockingQueue {
 
  private:
   std::deque<T> queue_;
+  std::mutex mutex_;
+  std::condition_variable cv_not_empty;
 };
 
 #endif  //_BLOCING_QUEUE_H_
