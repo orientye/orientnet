040738ef9aca6fcc75bd0e7be4a6153d48fb6ba3
orient <orientye@qq.com>
2021-12-09 21:39:17 +0800

    log

--- util/log/log.cc
+++ util/log/log.cc
@@ -1,1 +1,8 @@
 #include "util/log/log.h"
+
+void Log::trace(const char *s) {}
+void Log::debug(const char *s) {}
+void Log::info(const char *s) {}
+void Log::warn(const char *s) {}
+void Log::error(const char *s) {}
+void Log::fatal(const char *s) {}
