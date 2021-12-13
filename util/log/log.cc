a03c8172ce9340108b4e7d37fb5a24bbb670fe3f
orient <orientye@qq.com>
2021-12-13 21:59:27 +0800

    log

--- util/log/log.cc
+++ util/log/log.cc
@@ -1,11 +1,11 @@
 #include "util/log/log.h"
 
-void Logger::trace(const char *s) {}
-void Logger::debug(const char *s) {}
-void Logger::info(const char *s) {}
-void Logger::warn(const char *s) {}
-void Logger::error(const char *s) {}
-void Logger::fatal(const char *s) {}
+void Logger::trace(const char* s) {}
+void Logger::debug(const char* s) {}
+void Logger::info(const char* s) {}
+void Logger::warn(const char* s) {}
+void Logger::error(const char* s) {}
+void Logger::fatal(const char* s) {}
 
 void Logger::setLogLevel(LogLevel level) {}
-void Logger::setLogFile(const char *file_name) {}
+void Logger::setLogFile(const char* file_name) {}
--- util/log/log.h
+++ util/log/log.h
@@ -14,12 +14,12 @@ class Logger {
   };
 
  public:
-  void trace(const char *s);
-  void debug(const char *s);
-  void info(const char *s);
-  void warn(const char *s);
-  void error(const char *s);
-  void fatal(const char *s);
+  void trace(const char* s);
+  void debug(const char* s);
+  void info(const char* s);
+  void warn(const char* s);
+  void error(const char* s);
+  void fatal(const char* s);
 
   void setLogLevel(LogLevel level);
   void setLogFile(const char* file_name);
