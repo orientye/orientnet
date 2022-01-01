c5f7b6e96a189d62b92ff1693a277b49c74af9f7
orientye <orientye@qq.com>
2022-01-01 11:11:19 +0800

    platform

--- util/platform.h
+++ util/platform.h
@@ -7,7 +7,7 @@
     #include "platform/unix.h"
 #endif
 
-tn::FD open_file(const char* filename);
+tn::FD open_file(const char* filename, int flags, int mode);
 void close_file(tn::FD fd);
 
 #endif  // UTIL_PLATFORM_H_
