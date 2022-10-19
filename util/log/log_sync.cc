3eb324524e18eaa6f43f44cad49f52a07a4b2b9e
orientye <orientye@qq.com>
2022-10-19 22:31:08 +0800

    log

--- util/log/log_sync.cc
+++
@@ -1,8 +0,0 @@
-#include "util/log/log_sync.h"
-
-SyncLogger::SyncLogger(const char* file_name) {}
-SyncLogger::~SyncLogger() {}
-
-void SyncLogger::critical(const char* s) {}
-
-void SyncLogger::set_file(const char* filename) {}
