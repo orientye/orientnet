2bb69f81535ff10b849744e6d7b7b0ad9ef7d343
orientye <orientye@qq.com>
2023-02-10 20:19:58 +0800

    log

--- util/log/log.cc
+++ util/log/log.cc
@@ -16,7 +16,5 @@ void Logger::warn(const char* s) {}
 void Logger::error(const char* s) {}
 void Logger::fatal(const char* s) {}
 
-void Logger::set_config_file(const char* cf_filename) {}
-
 void Logger::set_level(LogLevel level) {}
 void Logger::set_file(const char* filename) {}
